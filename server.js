ac867b92fdbff59a1b46f7bd9d2dc85c6eb9c968
Gustavo Braz <gustavo.braz89@gmail.com>
2020-04-03 16:41:15 -0300

    Encapsula rotas

--- server.js
+++ server.js
@@ -4,32 +4,6 @@ app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000;');
 });
 
-app.get('/', (req, resp) => {
-  resp.send(`
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Casa do Código </h1>
-        </body>
-    </html>
-  `);
-});
-
-app.get('/livros', (req, resp) => {
-  resp.send(`
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Listagem de Livros! </h1>
-        </body>
-    </html>
-  `);
-});
-
 // const http = require('http');
 
 // const servidor = http.createServer((req, resp) => {
