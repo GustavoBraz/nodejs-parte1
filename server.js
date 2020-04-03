f20d07e0a3b189c0ec8edd4602a078e30b59c5ad
Gustavo Braz <gustavo.braz89@gmail.com>
2020-04-03 15:07:30 -0300

    Cria rotas usando o Express

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer((req, resp) => {
-  let html = '';
-  if (req.url == '/') {
-    html = `
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000;');
+});
+
+app.get('/', (req, resp) => {
+  resp.send(`
     <html>
         <head>
             <meta charset="utf-8">
@@ -12,9 +15,11 @@ const servidor = http.createServer((req, resp) => {
             <h1> Casa do Código </h1>
         </body>
     </html>
-    `;
-  } else if (req.url == '/livros') {
-    html = `
+  `);
+});
+
+app.get('/livros', (req, resp) => {
+  resp.send(`
     <html>
         <head>
             <meta charset="utf-8">
@@ -23,8 +28,36 @@ const servidor = http.createServer((req, resp) => {
             <h1> Listagem de Livros! </h1>
         </body>
     </html>
-    `;
-  }
-  resp.end(html);
+  `);
 });
-servidor.listen(3000);
+
+// const http = require('http');
+
+// const servidor = http.createServer((req, resp) => {
+//   let html = '';
+//   if (req.url == '/') {
+//     html = `
+//     <html>
+//         <head>
+//             <meta charset="utf-8">
+//         </head>
+//         <body>
+//             <h1> Casa do Código </h1>
+//         </body>
+//     </html>
+//     `;
+//   } else if (req.url == '/livros') {
+//     html = `
+//     <html>
+//         <head>
+//             <meta charset="utf-8">
+//         </head>
+//         <body>
+//             <h1> Listagem de Livros! </h1>
+//         </body>
+//     </html>
+//     `;
+//   }
+//   resp.end(html);
+// });
+// servidor.listen(3000);
