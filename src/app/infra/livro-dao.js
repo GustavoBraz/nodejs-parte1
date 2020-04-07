2fbd1e5bc9bd8a09b2933ad10174508c24bcb60a
Gustavo Braz <gustavo.braz89@gmail.com>
2020-04-07 17:10:08 -0300

    Utiliza DAO com Promises - then e catch

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,15 +1,21 @@
 class LivroDao {
+
   constructor(db) {
     this._db = db;
   }
 
-  lista(callback) {
-    this._db.all(
-      'SELECT * FROM livros',
-      (erro, resultados) => {
-        callback(erro, resultados);
-      }
-    )
+  lista() {
+    return new Promise((resolve, reject) => {
+      this._db.all(
+        'SELECT * FROM livros',
+        (erro, resultados) => {
+          if (erro) return reject('Não foi possível listar os livros!');
+
+          return resolve(resultados);
+        }
+      )
+
+    });
   }
 }
 
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -17,24 +17,14 @@ module.exports = (app) => {
   app.get('/livros', (req, resp) => {
 
     const livroDao = new LivroDao(db);
-    livroDao.lista((erro, resultados) => {
-
-      resp.marko(
+    livroDao.lista()
+      .then(livros => resp.marko(
         require('../views/livros/lista/lista.marko'),
         {
-          livros: resultados
+          livros: livros
         }
-      );
 
-    });
-
-    // db.all('SELECT * FROM livros', (erro, resultados) => {
-    //   resp.marko(
-    //     require('../views/livros/lista/lista.marko'),
-    //     {
-    //       livros: resultados
-    //     }
-    //   );
-    // });
+      ))
+      .catch(erro => console.log(erro));
   });
 };
